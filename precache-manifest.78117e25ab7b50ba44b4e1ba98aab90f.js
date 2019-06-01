af1ed3b0d5762373adc033cbdc2daa51c8d94de5
zhengxiaoyao0716 <1499383852@qq.com>
2019-06-02 00:50:12 +0800

    init

---
+++ precache-manifest.78117e25ab7b50ba44b4e1ba98aab90f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b0ebe72e14320b9a18645bb9c08e952",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9948b015984d32f6622c",
+    "url": "/static/css/main.4f529dfe.chunk.css"
+  },
+  {
+    "revision": "9aeacbe8a30c76327e23",
+    "url": "/static/js/2.bb389e42.chunk.js"
+  },
+  {
+    "revision": "9948b015984d32f6622c",
+    "url": "/static/js/main.36235768.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
