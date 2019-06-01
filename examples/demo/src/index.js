4d4a226650ad58faf263340258a85cf8fc108324
zhengxiaoyao0716 <1499383852@qq.com>
2019-06-02 01:59:00 +0800

    redirect '/' to '/Home' is foolish idea

--- examples/demo/src/index.js
+++ examples/demo/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Redirect, Route, RouteComponentProps } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import Home from './Home';
 import { scenes } from './Scene';
@@ -10,12 +10,7 @@ export const PUBLIC_URL = process.env.PUBLIC_URL; // eslint-disable-line no-unde
 ReactDOM.render(
   <BrowserRouter>
     <>
-      <Route exact path={`${PUBLIC_URL}/`} /* redirect the root page to `Home` */
-        component={({ location }: RouteComponentProps) => (<Redirect to={{ ...location, pathname: `${PUBLIC_URL}/${Home.displayName}` }} />)} />
-      <Route path={`${PUBLIC_URL}/${Home.displayName}`} component={Home} />
-
-      <Route exact path={`${PUBLIC_URL}/--start`} /* redirect `start` to first scene */
-        component={({ location }: RouteComponentProps) => (<Redirect to={{ ...location, pathname: `${PUBLIC_URL}/404` }} />)} />
+      <Route exact path={`${PUBLIC_URL}/`} component={Home} />
 
       {scenes.map(Scene => (
         <Route key={Scene.displayName} path={`${PUBLIC_URL}/scene/${Scene.displayName}`} component={Scene} />
