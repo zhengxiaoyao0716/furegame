70a05af916a86917d82e8827b509183be4e4fa18
zhengxiaoyao0716 <1499383852@qq.com>
2019-10-07 14:48:40 +0800

    add `back` button

--- examples/demo/src/Scene/index.js
+++ examples/demo/src/Scene/index.js
@@ -1,9 +1,26 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 import StageAndUI from './StageAndUI';
 import TextureAndSprite from './TextureAndSprite';
 import PreloadResource from './PreloadResource';
 import LogicDecoupling from './LogicDecoupling';
 import HighFrequency from './HighFrequency';
 import Physics from './Physics';
+
+const createScene = Component => {
+  const Scene = (props, context) => {
+    const history = useHistory();
+    return (
+      <>
+        {Component(props, context)}
+        <div id="back" onClick={() => history.goBack()}>BACK</div>
+      </>
+    );
+  };
+  Scene.displayName = Component.displayName;
+  Scene.displayText = Component.displayText;
+  return Scene;
+};
 
 export const scenes = [
   StageAndUI,
@@ -12,4 +29,4 @@ export const scenes = [
   PreloadResource,
   LogicDecoupling,
   Physics,
-];
+].map(createScene);
